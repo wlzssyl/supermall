f7e7d582942723b20d8f15a672813a23af9ef3df
wlzssyl <zhangboy7660@qq.com>
2020-08-03 14:55:38 +0800

    init config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack :{
+    resolve:{
+      alias:{
+        '@':'src',
+        'assets':'@/assets',
+        'components':'@/components',
+        'common':'@/common',
+        'network':'@/network',
+        'views':'@/views',
+      }
+    }
+  }
+}
