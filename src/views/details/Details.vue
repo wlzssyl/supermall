8183a81c8d60b7e422c26a4af4a19421aef292de
wlzssyl <zhangboy7660@qq.com>
2020-08-14 17:58:28 +0800

    详情页图片加载

--- src/views/details/Details.vue
+++ src/views/details/Details.vue
@@ -2,10 +2,12 @@
   <div class="detail-page">
     <top-bar class="detail-topbar"></top-bar>
     <!-- betterscroll滚动 -->
-    <better-scroll class="detail-content">
+    <better-scroll class="detail-content" ref="scroll">
       <detail-swiper :top-img="topImgArray"></detail-swiper>
       <items-base :item-base-info="itemBaseInfo"></items-base>
       <details-shop :shop-info="shopInfos"></details-shop>
+      <details-image :detail-image="detailImage" 
+        :detail-desc="detailDesc" @scrollRefresh="scRrefresh"></details-image>
     </better-scroll>
   </div>
 </template>
@@ -15,6 +17,7 @@ import TopBar from './detailschild/topbar'
 import DetailSwiper from './detailschild/DetailsSwiper'
 import ItemsBase from './detailschild/ItemsBase'
 import DetailsShop from './detailschild/DetailsShop'
+import DetailsImage from './detailschild/DetalisImage'
 
 import BetterScroll from 'components/common/BScroll/BetterScroll'
 
@@ -27,6 +30,7 @@ export default {
     DetailSwiper,
     ItemsBase,
     DetailsShop,
+    DetailsImage,
     BetterScroll
   },
   data() {
@@ -34,7 +38,9 @@ export default {
       id:null, //商品id
       topImgArray:null ,//详情页轮播图
       itemBaseInfo:{},  //商品基本信息
-      shopInfos:{}  //店家信息
+      shopInfos:{} , //店家信息
+      detailImage:[],  //商品详情图片
+      detailDesc:[]  //商品说明
     }
   },
   created() {
@@ -42,7 +48,7 @@ export default {
     this.id = this.$route.params.iid;
     //获取该id的数据
     getDetailsData(this.id).then((res) => {
-      //console.log(res);
+      console.log(res);
       //1.保存轮播图数据
       this.topImgArray = res.data.result.itemInfo.topImages;
 
@@ -52,8 +58,17 @@ export default {
         res.data.result.columns, 
         res.data.result.shopInfo.services);
       //3.保存店家信息数据
-        this.shopInfos = new Shop(res.data.result.shopInfo);
+      this.shopInfos = new Shop(res.data.result.shopInfo);
+      //4.保存商品图片信息和说明
+      this.detailImage = res.data.result.detailInfo.detailImage[0].list;
+      this.detailDesc[0] = res.data.result.detailInfo.desc;
+      this.detailDesc[1] = res.data.result.detailInfo.detailImage[0].key;
     }).catch((err) => {});
+  },
+  methods:{
+    scRrefresh(){
+      this.$refs.scroll.scroll.refresh();
+    }
   }
 }
 </script>
---
+++ src/views/details/detailschild/DetalisImage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="detali-image">
+    <div class="detail-desc">
+      {{detailDesc[0]}}
+    </div>
+    <div class="det-image">
+      <p>{{detailDesc[1]}}</p>
+      <img :src="value" v-for="value in detailImage" :key="value"
+        @load="iamgeLoad">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      imageCount:0
+    }
+  },
+  props:{
+    detailDesc:{
+      type:Array,
+      default() {
+        return []
+      }
+    },
+    detailImage:{
+      type:Array,
+      default() {
+        return []
+      }
+    }
+  },
+  methods:{
+    iamgeLoad(){//等图片加载完了刷新一下better-scroll
+      ++this.imageCount;
+      if(this.imageCount === this.detailImage.length)
+        this.$emit('scrollRefresh');
+    }
+  }
+}
+</script>
+
+<style>
+  .detali-image{
+    margin: 0 12px;
+  }
+  .det-image{
+    width: 100%;
+    margin: 0 auto;
+  }
+  .det-image img{
+    width: 100%;
+  }
+  .det-image p{
+    color: #444;
+    margin: 10px 0 4px 0;
+  }
+  .detail-desc{
+    margin-top: 20px;
+    font-size: 14px;
+    color: #888;
+  }
+</style>
